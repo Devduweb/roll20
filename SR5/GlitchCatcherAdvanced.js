eedca5f297b81391b8c1dc7b0ea76ab8a01aa2b9
Devduweb <contact@devduweb.com>
2017-01-07 14:42:33 +0100

    GlitchCatcher - Use the over half rule instead of over-or-equals

--- SR5/GlitchCatcherAdvanced.js
+++ SR5/GlitchCatcherAdvanced.js
@@ -1,14 +1,14 @@
 // This is the Glitch Catcher, modified to word for the "Advanced" SR5 character sheet
 on('chat:message', function (msg) {
-    
+
     if((msg.type != 'general' && msg.type != 'whisper') || msg.rolltemplate != 'shadowrun') {
         return;
     }
-    
+
     if(msg.content.indexOf('Initiative') >= 0) {
         return; //not a pooled roll, don't report glitches
     }
-    
+
     var poolSize;
     var dice;
     for(var k in msg.inlinerolls) {
@@ -18,16 +18,16 @@ on('chat:message', function (msg) {
             break;
         }
     }
-    
+
     //log("Dice:"+dice);
     //log("Pool Size"+poolSize);
-    
+
     var oneCount = 0;
     var hitCount = 0;
     var i = 0;
     var r;
     var glitchTreshold = Math.floor(poolSize/2);
-    
+
     for(i=0; i<dice.length; i++) {
         if(dice[i].v == 1) {
             oneCount++;
@@ -37,12 +37,12 @@ on('chat:message', function (msg) {
         }
     }
     log(msg.who + " rolled " + oneCount +" out of " + poolSize);
-    if(oneCount >= glitchTreshold && hitCount == 0) {
+    if(oneCount > glitchTreshold && hitCount == 0) {
         log(msg.who + " critically glitched");
         sendChat("player|"+msg.playerid, "/direct <big><strong>CRIT GLITCH!</strong></big>");
-    } else if(oneCount >= glitchTreshold){
+    } else if(oneCount > glitchTreshold){
         log(msg.who + " glitched");
         sendChat("player|"+msg.playerid, "/direct <big><strong>GLITCH!</strong></big>");
     }
-    
+
 });
